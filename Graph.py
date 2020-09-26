47f582cca8f72e91bb93f6e89e4a4d5e30794af2
Bright-Shadow <50991557+Bright-Shadow@users.noreply.github.com>
2020-09-26 14:23:17 -0400

    Add files via upload

--- Graph.py
+++ Graph.py
@@ -1,21 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from Equation import Expression
+import io
+import base64
 
-# Create the vectors X and Y
-x = np.array(range(10))
-input_equations = ['1/x']
-for equation in input_equations:
-    fn = Expression(equation, ["x"])
-    y = fn(x)
-    plt.plot(x, y)
 
-# Add X and y Label
-plt.xlabel('x axis')
-plt.ylabel('y axis')
+class Graph:
+    def input(self, equations: list):
+        # Create the vectors X and Y
+        x = np.array(range(10))
+        for equation in equations:
+            fn = Expression(equation, ["x"])
+            y = fn(x)
+            plt.plot(x, y)
 
-# Add a grid
-plt.grid(alpha=.4, linestyle='--')
+        # Add X and y Label
+        plt.xlabel('x axis')
+        plt.ylabel('y axis')
 
-# Show the plot
-plt.show()
+        # Add a grid
+        plt.grid(alpha=.4, linestyle='--')
+
+        # Show the plot
+        return plt
+
+    def fig_to_base64(self, fig):
+        img = io.BytesIO()
+        fig.savefig(img, format='png',
+                    bbox_inches='tight')
+        img.seek(0)
+
+        return base64.b64encode(img.getvalue())
+
+
+if __name__ == '__main__':
+    graph = Graph()
+    fig = graph.input(["x**2"])
+    fig.savefig('my_plot.png')
+    encoded = graph.fig_to_base64(fig)
+    my_html = '<img src="data:image/png;base64, {}">'.format(encoded.decode('utf-8'))
